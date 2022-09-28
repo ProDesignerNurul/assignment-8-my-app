12133a293baac833b7e29211ce94d1f15ac8ae3e
ProDesignerNurul <maxnewsbd@gmail.com>
2022-09-28 22:55:05 +0600

    load some data

---
+++ src/utilities/FakeData/FakeData.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const FakeData = () => {
+    const students = [
+        {name: 'Guljar Ahmed', descriptions: 'pore dibo', img: 'https://sponsored.chronicle.com/lifelonglearning/assets/UtzDNE3HUI/adults-learning-612x612-612x408.jpeg'},
+        {name: 'Guljar Ahmed', descriptions: 'pore dibo', img: 'https://hamptonprep.school/wp-content/uploads/2022/03/istockphoto-1278978402-612x612-1-612x408.jpg'},
+        {name: 'Guljar Ahmed', descriptions: 'pore dibo', img: 'https://aadi.org.in/assets/images/istockphoto-1226796079-612x408.jpg'},
+        {name: 'Guljar Ahmed', descriptions: 'pore dibo', img: 'https://aadi.org.in/assets/images/istockphoto-1029717906-612x408.jpg'},
+        {name: 'Guljar Ahmed', descriptions: 'pore dibo', img: 'https://practicalesg.com/wp-content/uploads/2021/11/image5_diversemeeting_original-612x408.jpg'},
+        {name: 'Guljar Ahmed', descriptions: 'pore dibo', img: 'https://media.istockphoto.com/photos/woman-studying-late-at-night-picture-id623201854?k=20&m=623201854&s=612x612&w=0&h=7bDc67tFFrfVpEr8xQvT7M_c6MxevVVBckheMJQTszk='}
+    ]
+    return (
+        <div>
+            <h2>Hello Bangladesh</h2>
+        </div>
+    );
+};
+
+export default FakeData;
